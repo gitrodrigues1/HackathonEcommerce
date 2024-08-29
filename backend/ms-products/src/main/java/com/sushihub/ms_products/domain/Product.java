1054bbc281f31b97666ae9c31a9a093a5e3dcbbf
Rafael <rafaelramalhosilveira@gmail.com>
2024-08-28 22:04:57 -0400

    Create product entity

---
+++ backend/ms-products/src/main/java/com/sushihub/ms_products/domain/Product.java
@@ -0,0 +1,29 @@
+package com.sushihub.ms_products.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name="product")
+@Entity(name="Product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Lob
+    private byte[] picture;
+    private String description;
+    private String price;
+
+    @Enumerated(EnumType.STRING)
+    private Size size;
+
+    private boolean promotion;
+
+}
