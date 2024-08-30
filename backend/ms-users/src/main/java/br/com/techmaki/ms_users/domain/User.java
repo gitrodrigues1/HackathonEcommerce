60161344b2f20d081f2fabe7460fafa955258307
NevesHenry <henriquereisneves@hotmail.com>
2024-08-30 15:35:50 -0300

    create User entity

---
+++ backend/ms-users/src/main/java/br/com/techmaki/ms_users/domain/User.java
@@ -0,0 +1,26 @@
+package br.com.techmaki.ms_users.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private boolean active;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
