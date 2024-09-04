25af0a3a82a933145c01a592905cb21192b109e1
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-09-04 15:21:17 -0300

    chore: change user repository folder

--- backend/ms-users/src/main/java/br/com/techmaki/ms_users/infrastructure/database/repository/UserRepository.java
+++ backend/ms-users/src/main/java/br/com/techmaki/ms_users/infrastructure/database/repository/UserRepository.java
@@ -1,12 +1,15 @@
-package br.com.techmaki.ms_users.repository;
+package br.com.techmaki.ms_users.infrastructure.database.repository;
 
-import br.com.techmaki.ms_users.domain.entities.User;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import br.com.techmaki.ms_users.domain.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     List<User> findAllByActiveTrue();
+    Optional<User> findByEmail(String email);
 }
