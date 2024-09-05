043b5031097adf3dd2b0719cb24f86763ced5479
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-09-05 19:41:22 -0300

    fix: update order repository

--- backend/src/main/java/com/techmaki/sushi_hub/orders/infrastructure/database/repositories/OrderRepository.java
+++ backend/src/main/java/com/techmaki/sushi_hub/orders/infrastructure/database/repositories/OrderRepository.java
@@ -3,9 +3,14 @@ package com.techmaki.sushi_hub.orders.infrastructure.database.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.techmaki.sushi_hub.orders.domain.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserId(Long userId);
+
+    @Query(value = "SELECT * FROM orders a INNER JOIN users b on a.user_id = b.id  WHERE b.id = :userId ORDER BY a.id", 
+        nativeQuery = true)
+    List<Order> findByUser(@Param("userId") Long userId);
 }
