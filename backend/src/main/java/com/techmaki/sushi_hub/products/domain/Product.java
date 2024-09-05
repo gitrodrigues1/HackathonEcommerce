82ed54532e627da13ffc67d96578801567ed5ec7
NevesHenry <henriquereisneves@hotmail.com>
2024-09-04 23:18:16 -0300

    Add products structure

---
+++ backend/src/main/java/com/techmaki/sushi_hub/products/domain/Product.java
@@ -0,0 +1,29 @@
+package com.techmaki.sushi_hub.products.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name="product")
+@Entity(name="Product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Lob
+    private byte[] picture;
+    private String description;
+    private String price;
+
+    @Enumerated(EnumType.STRING)
+    private ProductSize size;
+
+    private boolean promotion;
+
+}
