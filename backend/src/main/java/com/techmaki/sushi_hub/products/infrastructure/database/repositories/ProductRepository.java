b6c531e6d5cc63056aecfe4b1b1f518ace1a2f8d
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-09-05 19:45:41 -0300

    feat: add product repo and update entity

---
+++ backend/src/main/java/com/techmaki/sushi_hub/products/infrastructure/database/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.techmaki.sushi_hub.products.infrastructure.database.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.techmaki.sushi_hub.products.domain.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
