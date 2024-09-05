398e4f4e5b814ef4ac6e970f3fe0d05d42ae9f39
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-09-05 19:41:12 -0300

    fix: update order service

--- backend/src/main/java/com/techmaki/sushi_hub/orders/services/OrderService.java
+++ backend/src/main/java/com/techmaki/sushi_hub/orders/services/OrderService.java
@@ -1,21 +1,26 @@
 package com.techmaki.sushi_hub.orders.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.techmaki.sushi_hub.orders.domain.dtos.CreateOrderItemRequest;
 import com.techmaki.sushi_hub.orders.domain.dtos.CreateOrderRequest;
 import com.techmaki.sushi_hub.orders.domain.dtos.UpdateOrderItemRequest;
 import com.techmaki.sushi_hub.orders.domain.dtos.UpdateOrderRequest;
 import com.techmaki.sushi_hub.orders.domain.dtos.UpdateOrderStatusRequest;
 import com.techmaki.sushi_hub.orders.domain.entities.Order;
 import com.techmaki.sushi_hub.orders.domain.entities.OrderItem;
+import com.techmaki.sushi_hub.orders.domain.entities.OrderStatus;
 import com.techmaki.sushi_hub.orders.infrastructure.database.repositories.OrderItemRepository;
 import com.techmaki.sushi_hub.orders.infrastructure.database.repositories.OrderRepository;
-
-import jakarta.transaction.Transactional;
+import com.techmaki.sushi_hub.products.domain.Product;
+import com.techmaki.sushi_hub.products.infrastructure.database.repositories.ProductRepository;
+import com.techmaki.sushi_hub.users.domain.entity.User;
+import com.techmaki.sushi_hub.users.infrastructure.repository.UserRepository;
 
 @Service
 public class OrderService {
@@ -23,17 +28,30 @@ public class OrderService {
     private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final OrderItemRepository orderItemRepository;
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
     
-    public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository) {
+    public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository, UserRepository userRepository, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
         this.orderItemRepository = orderItemRepository;
+        this.userRepository = userRepository;
+        this.productRepository = productRepository;
     }
 
-    @Transactional
     public Order createOrder(CreateOrderRequest request) {
         try {
-            Order order = request.toModel();
+            User user = userRepository.findById(request.userId()).get();
+            Order order = Order.builder()
+                .user(user)
+                .orderDate(LocalDateTime.now())
+                .status(OrderStatus.WAITING_PAYMENT)
+                .orderItems(request.items().stream().map(x -> checkProduct(x)).toList())
+                .build();
+
+            order = calculateOrder(order);
+            //search product by product id
             orderRepository.save(order);
+
             orderItemRepository.saveAll(order.getOrderItems());
             return order;
         } catch (Exception e) {
@@ -63,7 +81,7 @@ public class OrderService {
     }
 
     public List<Order> getAllOrdersByClient(Long id) {
-        return orderRepository.findByUserId(id);
+        return orderRepository.findByUser(id);
     }
 
     private static List<OrderItem> updateOrderItems(List<OrderItem> items, List<UpdateOrderItemRequest> itemsRequest) {
@@ -91,4 +109,13 @@ public class OrderService {
         order.setTotalAmount(total);
         return order;
     }
+
+    private OrderItem checkProduct(CreateOrderItemRequest itemRequest) {
+        Product product = productRepository.findById(itemRequest.productId())
+            .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+        OrderItem orderItem = OrderItem.toModel(itemRequest);
+        orderItem.setProductName(product.getName());
+        return orderItem;
+    }
+
 }
