c3fd9ca04632c489a8320f244854055363695e3b
NevesHenry <henriquereisneves@hotmail.com>
2024-09-04 22:54:37 -0300

    Add basic Spring structure

---
+++ backend/src/main/java/com/techmaki/sushi_hub/SushiHubApplication.java
@@ -0,0 +1,13 @@
+package com.techmaki.sushi_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/techmaki/sushi_hub/SushiHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmaki.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
