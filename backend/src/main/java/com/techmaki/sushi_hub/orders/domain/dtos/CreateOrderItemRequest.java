c83de4a8b496318c1b722c56805fd12fe2a95762
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-09-05 19:39:17 -0300

    fix: update dto's

--- backend/src/main/java/com/techmaki/sushi_hub/orders/domain/dtos/CreateOrderItemRequest.java
+++ backend/src/main/java/com/techmaki/sushi_hub/orders/domain/dtos/CreateOrderItemRequest.java
@@ -1,20 +1,12 @@
 package com.techmaki.sushi_hub.orders.domain.dtos;
 
-import com.techmaki.sushi_hub.orders.domain.entities.OrderItem;
-
 public record CreateOrderItemRequest(
 
     long productId,
     int quantity,
     double price,
-    double totlaPrice
+    double discount,
+    double totalPrice
 ) {
 
-    public OrderItem toModel() {
-        return OrderItem.builder()
-            .productId(productId)
-            .quantity(quantity)
-            .price(price)
-            .build();
-    }
 }
--- backend/src/main/java/com/techmaki/sushi_hub/orders/domain/dtos/CreateOrderRequest.java
+++ backend/src/main/java/com/techmaki/sushi_hub/orders/domain/dtos/CreateOrderRequest.java
@@ -1,22 +1,11 @@
 package com.techmaki.sushi_hub.orders.domain.dtos;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-import com.techmaki.sushi_hub.orders.domain.entities.Order;
-import com.techmaki.sushi_hub.orders.domain.entities.OrderStatus;
-
 public record CreateOrderRequest(
-    String orderDate, 
+    Long userId,
     List<CreateOrderItemRequest> items
 ) {
-    
-    public Order toModel() {
-        return Order.builder()
-            .orderDate(LocalDateTime.now())
-            .status(OrderStatus.WAITING_PAYMENT)
-            .orderItems(items.stream().map(CreateOrderItemRequest::toModel).toList())
-            .build();
-    }
+
 
 }
