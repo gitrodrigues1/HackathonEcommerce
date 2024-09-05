aeee3eb2d7dfd6710ee556495770bcd439d434e8
NevesHenry <henriquereisneves@hotmail.com>
2024-09-05 10:46:38 -0300

    Add BearerTokenResolver Bean

--- backend/src/main/java/com/techmaki/sushi_hub/users/infrastructure/securityconfig/SecurityConfig.java
+++ backend/src/main/java/com/techmaki/sushi_hub/users/infrastructure/securityconfig/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.techmaki.sushi_hub.users.infrastructure.securityconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +15,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
+import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.JWK;
@@ -25,7 +29,7 @@ import com.nimbusds.jose.proc.SecurityContext;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-
+    @Autowired
     private final RsaKeyProperties rsaKeys;
 
     public SecurityConfig(RsaKeyProperties rsaKeys) {
@@ -35,7 +39,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> {
                 auth.requestMatchers(HttpMethod.POST, "users/subscribe").permitAll();
                 auth.requestMatchers(HttpMethod.POST, "auth/token").permitAll();
@@ -64,5 +68,13 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public BearerTokenResolver customBearerTokenResolver() {
+        DefaultBearerTokenResolver resolver = new DefaultBearerTokenResolver();
+        resolver.setAllowFormEncodedBodyParameter(true); // Allow tokens in request parameters
+        resolver.setAllowUriQueryParameter(true); // Allow tokens in URI query parameters
+        return resolver;
     }
 }
