de1337c48bf73f9fd98b2f0f7d5bbfc9c792f9eb
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-09-05 19:35:28 -0300

    chore: add data.sql for tests

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO products values 
+(1, 'Kit Sushi Hub 20pcs', 'Kit Sushi Pequeno', 10.0, 'SMALL'),
+(2, 'Kit Sushi Hub 30pcs', 'Kit Sushi Medio', 20.0,  'MEDIUM'),
+(3, 'Kit Sushi Hub 50pcs', 'Kit Sushi Grande', 30.0,  'LARGE');
