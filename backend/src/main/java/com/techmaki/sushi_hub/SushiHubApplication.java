6f995e4f4cd192b44065b111b8a248c2f4553d8d
NevesHenry <henriquereisneves@hotmail.com>
2024-09-05 00:00:02 -0300

    Fix properties

--- backend/src/main/java/com/techmaki/sushi_hub/SushiHubApplication.java
+++ backend/src/main/java/com/techmaki/sushi_hub/SushiHubApplication.java
@@ -1,9 +1,12 @@
 package com.techmaki.sushi_hub;
 
+import com.techmaki.sushi_hub.users.infrastructure.securityconfig.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class SushiHubApplication {
 
 	public static void main(String[] args) {
--- backend/src/test/java/com/techmaki/sushi_hub/SushiHubApplicationTests.java
+++ backend/src/test/java/com/techmaki/sushi_hub/SushiHubApplicationTests.java
@@ -1,4 +1,4 @@
-package com.techmaki.backend;
+package com.techmaki.sushi_hub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
