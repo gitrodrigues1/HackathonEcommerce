fef449d82c6f8b36e9d5feb13d0e7e76a8f5bc9d
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-09-09 18:21:50 -0300

    chore: update data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,9 @@
-INSERT INTO products values 
-(1, 'Kit Sushi Hub 20pcs', 'Kit Sushi Pequeno', 10.0, 'SMALL'),
-(2, 'Kit Sushi Hub 30pcs', 'Kit Sushi Medio', 20.0,  'MEDIUM'),
-(3, 'Kit Sushi Hub 50pcs', 'Kit Sushi Grande', 30.0,  'LARGE');
+INSERT INTO products ( name, description, price, size, is_active) VALUES 
+( 'Kit Sushi Hub 20pcs', 'Kit Sushi Pequeno', 10.0, 'SMALL', TRUE),
+( 'Kit Sushi Hub 30pcs', 'Kit Sushi Medio', 20.0,  'MEDIUM', TRUE),
+( 'Kit Sushi Hub 50pcs', 'Kit Sushi Grande', 30.0,  'LARGE', TRUE);
+
+-- INSERT INTO users (id, name, email, password, role, active) values 
+-- (1, 'John Doe', 'john@mail.com', '123456', 'ADMIN', TRUE),
+-- (2, 'Tonny Stark', 'tonny@mail.com', '123456', 'ADMIN', TRUE),
+-- (3, 'Mariana', 'mary@mail.com', '123456', 'ADMIN', TRUE);
