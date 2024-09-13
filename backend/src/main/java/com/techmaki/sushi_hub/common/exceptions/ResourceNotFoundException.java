204c3d06182fd496d38eb22be4b0c2c398b902fe
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-09-12 21:15:08 -0300

    feat: add exception

---
+++ backend/src/main/java/com/techmaki/sushi_hub/common/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.techmaki.sushi_hub.common.exceptions;
+
+public class ResourceNotFoundException extends Exception{
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
